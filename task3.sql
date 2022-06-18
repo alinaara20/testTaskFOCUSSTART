6ffaca1b72d76c4ab8e2b4e1e40fc5c4cc50d6b5
alinaara20 <alinaara400@gmail.com>
2022-06-19 00:46:09 +0700

    done

---
+++ task3.sql
@@ -0,0 +1,12 @@
+/*Подготовьте скрипты заполнения таблиц тестовыми данными, достаточными для выполнения заданий ниже.*/
+INSERT INTO products VALUES (4, 2, 'Депозитный договор с Сидоровым И.П.', 1, '2015.06.01', null);
+INSERT INTO accounts VALUES (4, 'Депозитный счет для Сидоровым И.П.', 2000, 1, '2015.06.01', null, 4, '45502810401020000033');
+INSERT INTO records VALUES (16, 0, 2000, 4, '2017.10.21');
+INSERT INTO records VALUES (17, 0, 2000, 4, '2022.06.01');
+INSERT INTO records VALUES (18, 1, 2000, 4, '2022.06.07');
+INSERT INTO records VALUES (19, 1, 2000, 4, '2022.05.29');
+INSERT INTO records VALUES (20, 0, 1500, 4, '2022.05.29');
+INSERT INTO products VALUES (5, 1, 'Кредитный договор с Петровым С.И.', 3, '2022.06.01', null);
+INSERT INTO accounts VALUES (5, 'Кредитный счет для Петровым С.И.', 0, 3, '2022.06.01', null, 5, '45502810401088000033');
+INSERT INTO records VALUES (21, 1, 3000, 5, '2022.06.01');
+INSERT INTO records VALUES (22, 0, 3000, 5, '2022.06.04');
